40c0b22156dd6416905e740509d41dfe23e22e44
Iryna_Nikalayeva <irinnik92@gmail.com>
2015-11-20 19:53:32 +0300

    adding module 3 task 4

---
+++ src/module3/task4/ListsRunner.java
@@ -0,0 +1,27 @@
+package module3.task4;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class ListsRunner {
+
+    public static void main(String args[]){
+        List<Integer> arrayList = new ArrayList<Integer>();
+        LinkedList<Integer> linkedList = new LinkedList<Integer>();
+        int listSize = 10000;
+        int searchValue = 9999;
+
+        ListsOperations listsOperations = new ListsOperations();
+
+        System.out.println("ArrayList: Time for add, ms: " + listsOperations.getTimeForAdd(arrayList, listSize));
+        System.out.println("LinkedList: Time for add, ms: " + listsOperations.getTimeForAdd(linkedList, listSize));
+
+        System.out.println("ArrayList: Time for detele, ms: " + listsOperations.getTimeForDelete(arrayList, listSize));
+        System.out.println("LinkedList: Time for delete, ms: " + listsOperations.getTimeForDelete(linkedList, listSize));
+
+        System.out.println("ArrayList: Time for search, ms: " + listsOperations.getTimeForSearch(arrayList, listSize, searchValue));
+        System.out.println("LinkedList: Time for search, ms: " + listsOperations.getTimeForSearch(arrayList, listSize, searchValue));
+    }
+}
